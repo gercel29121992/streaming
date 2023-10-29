3bb983c312e97c9f33efffd36ff5c2d3185756ac
gercel29121992 <gercelluciano@hotmail.com>
2023-10-28 20:38:55 -0400

    integracion con la plantilla html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+ 
+declare var $ :any;
+declare function HOME_INIT([]):any;  
+declare function INIT_SWPER([]):any;  
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Streaming';
+  constructor(){
+
+  }
+  ngOnInit(): void{
+
+   setTimeout(() => {
+    INIT_SWPER($)
+    HOME_INIT($);
+   }, 50);
+
+  }
 }
